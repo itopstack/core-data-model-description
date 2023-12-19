321408f741ff5c1369e66b85bf9eb821948d0a94
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-12-19 15:57:47 +0700

    Build with Carthage

---
+++ CoreDataModelDescription/CoreDataModelDescription/CoreDataModelDescription.h
@@ -0,0 +1,18 @@
+//
+//  CoreDataModelDescription.h
+//  CoreDataModelDescription
+//
+//  Created by Kittisak Phetrungnapha on 19/12/2566 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreDataModelDescription.
+FOUNDATION_EXPORT double CoreDataModelDescriptionVersionNumber;
+
+//! Project version string for CoreDataModelDescription.
+FOUNDATION_EXPORT const unsigned char CoreDataModelDescriptionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataModelDescription/PublicHeader.h>
+
+
